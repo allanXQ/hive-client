0f9af57be97130d9f221b72bef7139ddcad44ee8
Allan Juma <allancjuma@gmail.com>
2023-11-13 09:30:56 +0300

    update

--- src/utils/googleAuth.js
+++
@@ -1,7 +0,0 @@
-import { signInWithPopup } from "firebase/auth";
-import { firebaseAuth, firebaseAuthProvider } from "./firebase";
-import { loginSuccess } from "redux/features/user/userSlice";
-// import { useDispatch } from "react-redux";
-// import { apiCall } from "redux/async/asyncThunk";
-
-// export default GoogleSignIn;
