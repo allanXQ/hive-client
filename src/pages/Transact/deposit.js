f94d430adf16e24e69cac2a9a31531426a105b8c
Allan Juma <allancjuma@gmail.com>
2023-11-13 17:48:58 +0300

    instaructions and layout update

--- src/pages/Transact/deposit.js
+++ src/pages/Transact/deposit.js
@@ -1,7 +1,14 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
+import useResponsive from "Hooks/useResponsive";
 import DepositForm from "components/forms/models/deposit";
 
 const Deposit = () => {
+  const { isSm, isMd, isXl } = useResponsive();
+  const overviewWidth = isSm
+    ? "100vw"
+    : isXl
+    ? `calc(100vw - 200px)`
+    : `calc(100vw - 200px)`;
   return (
     <Box
       sx={{
@@ -11,26 +18,36 @@ const Deposit = () => {
         justifyContent: "center",
         gap: "1rem",
         height: "100vh",
+        width: overviewWidth,
       }}
     >
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          gap: "1rem",
-        }}
-      >
+      <Stack direction={isMd ? "column" : "row"} spacing={4}>
+        <Box sx={{ flexGrow: 1 }}>
+          <Typography variant="h6">Instructions</Typography>
+          <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+            <Typography variant="bodySmall">
+              1. Input your mpesa phone number
+            </Typography>
+            <Typography variant="bodySmall">
+              2. Input amount you wish to deposit(10 minimum)
+            </Typography>
+            <Typography variant="bodySmall">
+              3. Submit the form and wait for a prompt from Mpesa.
+            </Typography>
+            <Typography variant="bodySmall">
+              4. You will receive a prompt from Mpesa(Organisation: tinypesa)
+            </Typography>
+            <Typography variant="bodySmall">
+              5. Enter your Mpesa pin and submit
+            </Typography>
+            <Typography variant="bodySmall">
+              6. You will receive a confirmation message from Mpesa and Hive
+            </Typography>
+          </Box>
+        </Box>
         <DepositForm />
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          gap: "0.3rem",
-        }}
-      >
-        <Typography variant="bodySmallBold">© 2023 Hive</Typography>
-      </Box>
+      </Stack>
+      {/* <Typography variant="bodySmallBold">© 2023 Hive</Typography> */}
     </Box>
   );
 };
