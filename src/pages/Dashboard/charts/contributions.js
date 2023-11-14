ee5fb2cb2c8677710ff766bdcd20fd1c83251cd2
Allan Juma <allancjuma@gmail.com>
2023-11-14 06:09:09 +0300

    dashboard update

--- src/pages/Dashboard/charts/contributions.js
+++ src/pages/Dashboard/charts/contributions.js
@@ -1,99 +1,165 @@
-import React from "react";
-import { Pie } from "react-chartjs-2";
-import "chart.js/auto";
-import ChartDataLabels from "chartjs-plugin-datalabels";
-import { Box, Typography } from "@mui/material";
-import * as colors from "@mui/material/colors";
+// import React from "react";
+// import { Pie } from "react-chartjs-2";
+// import "chart.js/auto";
+// import ChartDataLabels from "chartjs-plugin-datalabels";
+// import { Box, Typography } from "@mui/material";
+// import * as colors from "@mui/material/colors";
 
-const Contributions = () => {
-  const colorNames = [
-    "red",
-    "pink",
-    "purple",
-    "deepPurple",
-    "indigo",
-    "blue",
-    "lightBlue",
-    "cyan",
-    "teal",
-    "green",
-    "lightGreen",
-    "lime",
-    "yellow",
-    "amber",
-    "orange",
-    "deepOrange",
-    "brown",
-    "grey",
-    "blueGrey",
-  ];
+// const Contributions = () => {
+//   const colorNames = [
+//     "red",
+//     "pink",
+//     "purple",
+//     "deepPurple",
+//     "indigo",
+//     "blue",
+//     "lightBlue",
+//     "cyan",
+//     "teal",
+//     "green",
+//     "lightGreen",
+//     "lime",
+//     "yellow",
+//     "amber",
+//     "orange",
+//     "deepOrange",
+//     "brown",
+//     "grey",
+//     "blueGrey",
+//   ];
 
-  const getRandomColor = (colorType) => {
-    let colorIndex = Math.floor(Math.random() * colorNames.length);
-    let colorName = colorNames[colorIndex];
-    return colors[colorName][colorType];
-  };
+//   const getRandomColor = (colorType) => {
+//     let colorIndex = Math.floor(Math.random() * colorNames.length);
+//     let colorName = colorNames[colorIndex];
+//     return colors[colorName][colorType];
+//   };
 
-  const backgroundColors = colorNames.map((name) => getRandomColor(500));
+//   const backgroundColors = colorNames.map((name) => getRandomColor(500));
+
+//   const data = {
+//     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+//     datasets: [
+//       {
+//         data: [12, 19, 3, 5, 2, 3],
+//         backgroundColor: backgroundColors,
+//         borderColor: backgroundColors,
+//         borderWidth: 1,
+//       },
+//     ],
+//   };
+
+//   const options = {
+//     plugins: {
+//       datalabels: {
+//         color: "#fff",
+//         textAlign: "center",
+//         font: {
+//           weight: "bold",
+//         },
+//         formatter: (value, context) => {
+//           // Extract the label from the dataset
+//           let label = context.chart.data.labels[context.dataIndex];
+//           // Combine the label and value to display both
+//           return label + "\n" + value;
+//         },
+//       },
+//       // legend: {
+//       //   display: false,
+//       // },
+//     },
+//   };
+
+//   return (
+//     <Box
+//       sx={{
+//         // padding: 0,
+//         // margin: 0,
+//         display: "flex",
+//         flexDirection: "column",
+//         justifyContent: "center",
+//         alignItems: "center",
+//         gap: 2,
+//       }}
+//     >
+//       <Typography variant="h6">Contributions</Typography>
+//       <Box
+//         sx={{
+//           width: "15vw",
+//           display: "flex",
+//           justifyContent: "center",
+//           alignItems: "center",
+//         }}
+//       >
+//         <Pie data={data} options={options} plugins={[ChartDataLabels]} />;
+//       </Box>
+//     </Box>
+//   );
+// };
+
+// export default Contributions;
+
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { useTheme } from "@mui/material";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const BarChart = () => {
+  const theme = useTheme();
 
   const data = {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    labels: [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+    ],
     datasets: [
       {
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: backgroundColors,
-        borderColor: backgroundColors,
-        borderWidth: 1,
+        label: "Contributions",
+        data: [65, 59, 80, 81, 56, 55, 40, 20],
+        backgroundColor: theme.palette.primary.main,
+        // borderColor: "rgba(255, 99, 132, 1)",
+        borderWidth: 0,
       },
     ],
   };
 
   const options = {
-    plugins: {
-      datalabels: {
-        color: "#fff",
-        textAlign: "center",
-        font: {
-          weight: "bold",
-        },
-        formatter: (value, context) => {
-          // Extract the label from the dataset
-          let label = context.chart.data.labels[context.dataIndex];
-          // Combine the label and value to display both
-          return label + "\n" + value;
-        },
+    scales: {
+      y: {
+        beginAtZero: true,
       },
-      // legend: {
-      //   display: false,
-      // },
     },
+    maintainAspectRatio: false,
   };
-
   return (
-    <Box
-      sx={{
-        // padding: 0,
-        // margin: 0,
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        gap: 2,
-      }}
-    >
-      <Typography variant="h6">Contributions</Typography>
-      <Box
-        sx={{
-          width: "15vw",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Pie data={data} options={options} plugins={[ChartDataLabels]} />;
-      </Box>
-    </Box>
+    // <>
+    //   <div className="header">
+    //     <h1 className="title">Bar Chart</h1>
+    //   </div>
+    <Bar data={data} options={options} height={400} />
+    // </>
   );
 };
 
-export default Contributions;
+export default BarChart;
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,13 +1,22 @@
-import { Box } from "@mui/material";
+import { Avatar, Box, Card, Grid, Stack, Typography } from "@mui/material";
 import React from "react";
 import Memberships from "./grids/memberships";
 import Deposits from "./grids/deposits";
 import Contributions from "./charts/contributions";
 import Overview from "./overview";
 import useResponsive from "Hooks/useResponsive";
+import { MuiButton } from "components/common/Button";
+import { useSelector } from "react-redux";
+import { selectTheme } from "redux/features/app/configSlice";
+import { useTheme } from "@emotion/react";
+import useUserData from "Hooks/useUserData";
+import BarChart from "./charts/contributions";
 
 const Dashboard = React.memo(() => {
   const { isSm, isXl } = useResponsive();
+  const currentTheme = useSelector(selectTheme);
+  const theme = useTheme();
+  const userData = useUserData();
   const overviewWidth = isSm
     ? "100vw"
     : isXl
@@ -15,51 +24,122 @@ const Dashboard = React.memo(() => {
     : `calc(100vw - 220px)`;
   const chartsWidth = `calc(100vw - ${overviewWidth})`;
 
+  const stats = [
+    {
+      name: "Balance",
+      value: userData?.accountBalance || 0,
+    },
+    {
+      name: "Contributions",
+      value: userData?.totalContributions || 0,
+    },
+    {
+      name: "Loans",
+      value: userData?.activeLoans || 0,
+    },
+    {
+      name: "Memberships",
+      value: userData?.totalWithdrawals || 0,
+    },
+  ];
+
   return (
-    <Box
+    <Grid
+      container
       sx={{
-        display: "flex",
-        flexWrap: "wrap",
-        flexDirection: {
-          xs: "column",
-          lg: "row",
-        },
-        paddingTop: 2,
-        // width: {
-        //   xs: "100vw",
-        //   sm: overviewWidth,
-        // },
+        width: overviewWidth,
+        px: 4,
       }}
     >
-      <Box
+      <Grid
+        item
         sx={{
           display: "flex",
-          flexDirection: "column",
-          // flexGrow: 1,
           gap: 5,
-          border: "0.5px solid gray",
-          padding: 2,
-          width: {
-            xs: "100vw",
-            sm: overviewWidth,
-          },
         }}
       >
-        <Overview />
-        <Deposits />
-      </Box>
-      <Box
-        sx={{
-          width: 300, //chartsWidth,
-          display: "flex",
-          padding: 2,
-        }}
-      >
-        <Memberships />
-
-        {/* <Contributions /> */}
-      </Box>
-    </Box>
+        <Card
+          sx={{
+            width: 350,
+            height: 300,
+            border: "1px solid #e0e0e0",
+            backgroundColor:
+              currentTheme === "light"
+                ? theme.palette.bgColor.light
+                : theme.palette.bgColor.dark,
+          }}
+        >
+          <Stack
+            direction="row"
+            spacing={4}
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              // justifyContent: "center",
+              p: 3,
+            }}
+          >
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <Typography variant="h6" sx={{ mb: 2 }}>
+                John Doe
+              </Typography>
+              <MuiButton
+                variant="contained"
+                content="Edit Profile"
+                sx={{
+                  width: 150,
+                }}
+              />
+            </Box>
+            <Avatar
+              alt="userData?.username"
+              src="https://mui.com/static/images/avatar/1.jpg" //{userData?.photoURL}
+              sx={{
+                width: 80,
+                height: 80,
+              }}
+            />
+          </Stack>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              gap: { xs: 2, sm: 4 },
+              flexWrap: "wrap",
+              flexGrow: 1,
+              flexFlow: "row wrap",
+              p: 3,
+            }}
+          >
+            {stats.map((stat) => (
+              <Box
+                key={stat.name}
+                sx={{
+                  width: 100,
+                }}
+              >
+                <Typography variant="body2">{stat.value}</Typography>
+                <Typography variant="body1">{stat.name}</Typography>
+              </Box>
+            ))}
+          </Box>
+        </Card>
+        <Card
+          sx={{
+            width: 350,
+            height: 300,
+            border: "1px solid #e0e0e0",
+            backgroundColor:
+              currentTheme === "light"
+                ? theme.palette.bgColor.light
+                : theme.palette.bgColor.dark,
+          }}
+        >
+          <BarChart />
+        </Card>
+      </Grid>
+    </Grid>
   );
 });
 
