212c96931da40cff5ff6603cbcb268f5cfd16055
Allan Juma <allancjuma@gmail.com>
2023-11-15 04:32:48 +0300

    dash layout 1 complete

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -53,7 +53,7 @@ const Dashboard = React.memo(() => {
           md: "repeat(2, 1fr)",
           lg: "repeat(3, 1fr)",
         },
-        gap: 5,
+        gap: 2,
       }}
     >
       <Grid item>
@@ -78,7 +78,11 @@ const Dashboard = React.memo(() => {
           >
             <Grid
               item
-              sx={{ display: "grid", gridTemplateColumns: "repeat(1, 1fr)" }}
+              sx={{
+                display: "grid",
+                gap: 2,
+                gridTemplateColumns: "repeat(1, 1fr)",
+              }}
             >
               <Typography variant="h6" sx={{ fontWeight: 600 }}>
                 John Doe
@@ -88,6 +92,7 @@ const Dashboard = React.memo(() => {
                 content="Edit Profile"
                 sx={{
                   width: "100%",
+                  maxHeight: "90%",
                 }}
               />
             </Grid>
@@ -120,9 +125,12 @@ const Dashboard = React.memo(() => {
                   flexDirection: "column",
                   gap: 1,
                   alignItems: "center",
+                  justifyContent: "center",
                 }}
               >
-                <Typography variant="h6">{stat.value}</Typography>
+                <Typography variant="h3" color="primary.main">
+                  {stat.value}
+                </Typography>
 
                 <Typography variant="h6">{stat.name}</Typography>
               </Grid>
@@ -134,7 +142,7 @@ const Dashboard = React.memo(() => {
         <Card
           sx={{
             // maxWidth: 350,
-            maxHeight: 313,
+            maxHeight: 328,
             border: "1px solid #e0e0e0",
             backgroundColor:
               currentTheme === "light"
@@ -150,87 +158,267 @@ const Dashboard = React.memo(() => {
         sx={{
           display: "grid",
           gridTemplateColumns: "repeat(1, 1fr)",
-          gridTemplateRows: "repeat(2, 1fr)",
-          gap: 5,
+          gridTemplateRows: "repeat(3, 1fr)",
+          gap: 2,
         }}
       >
         <Card
           sx={{
-            // width: "100%",
-            // height: 300,
+            p: 1,
             border: "1px solid #e0e0e0",
             backgroundColor:
               currentTheme === "light"
                 ? theme.palette.bgColor.light
                 : theme.palette.bgColor.dark,
+            display: "grid",
+            gap: 1,
           }}
         >
-          <Grid container>
-            <Grid
-              item
-              sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              variant="bodySmallBold"
+              sx={
+                {
+                  // fontWeight: 600,
+                }
+              }
             >
-              <Typography variant="h6" sx={{ fontWeight: 600 }}>
-                John Doe
-              </Typography>
-              <MuiButton
-                variant="contained"
-                content="Edit Profile"
-                sx={{
-                  width: "100%",
-                }}
-              />
-            </Grid>
-            <Grid item sx={{ display: "flex", flexDirection: "column" }}>
-              <Avatar
-                alt="userData?.username"
-                src="https://mui.com/static/images/avatar/1.jpg" //{userData?.photoURL}
-                sx={{
-                  width: 80,
-                  height: 80,
-                }}
-              />
-            </Grid>
-          </Grid>
+              RECENT TRANSACTIONS
+            </Typography>
+            <MuiButton
+              variant="text"
+              content="See All"
+              sx={{
+                margin: 0,
+                padding: 0,
+              }}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography variant="bodySmallBold">Type</Typography>
+              <Typography variant="bodySmallBold">Deposit</Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography variant="bodySmallBold">Amount</Typography>
+              <Typography variant="bodySmallBold">4000</Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography variant="bodySmallBold">Status</Typography>
+              <Typography variant="bodySmallBold">Failed</Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography variant="bodySmallBold">Date</Typography>
+              <Typography variant="bodySmallBold">12/12/2021</Typography>
+            </Box>
+          </Box>
         </Card>
         <Card
           sx={{
-            // width: "100%",
-            // height: 300,
+            p: 1,
             border: "1px solid #e0e0e0",
             backgroundColor:
               currentTheme === "light"
                 ? theme.palette.bgColor.light
                 : theme.palette.bgColor.dark,
+            display: "grid",
+            gap: 1,
           }}
         >
-          <Grid container>
-            <Grid
-              item
-              sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              variant="bodySmallBold"
+              sx={
+                {
+                  // fontWeight: 600,
+                }
+              }
             >
-              <Typography variant="h6" sx={{ fontWeight: 600 }}>
-                John Doe
-              </Typography>
-              <MuiButton
-                variant="contained"
-                content="Edit Profile"
-                sx={{
-                  width: "100%",
-                }}
-              />
-            </Grid>
-            <Grid item sx={{ display: "flex", flexDirection: "column" }}>
-              <Avatar
-                alt="userData?.username"
-                src="https://mui.com/static/images/avatar/1.jpg" //{userData?.photoURL}
-                sx={{
-                  width: 80,
-                  height: 80,
-                }}
-              />
-            </Grid>
-          </Grid>
+              UPCOMING OBLIGATIONS
+            </Typography>
+            <MuiButton
+              variant="text"
+              content="See All"
+              sx={{
+                margin: 0,
+                padding: 0,
+              }}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography variant="bodySmallBold">Chama</Typography>
+              <Typography variant="bodySmallBold">Chama 1</Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography variant="bodySmallBold">Type</Typography>
+              <Typography variant="bodySmallBold">Wellfare</Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography variant="bodySmallBold">Amount</Typography>
+              <Typography variant="bodySmallBold">100</Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography variant="bodySmallBold">Due</Typography>
+              <Typography variant="bodySmallBold">12/12/2021</Typography>
+            </Box>
+          </Box>
+        </Card>
+        <Card
+          sx={{
+            p: 1,
+            border: "1px solid #e0e0e0",
+            backgroundColor:
+              currentTheme === "light"
+                ? theme.palette.bgColor.light
+                : theme.palette.bgColor.dark,
+            display: "grid",
+            gap: 1,
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              variant="bodySmallBold"
+              sx={
+                {
+                  // fontWeight: 600,
+                }
+              }
+            >
+              ACTIVE LOANS
+            </Typography>
+            <MuiButton
+              variant="text"
+              content="See All"
+              sx={{
+                margin: 0,
+                padding: 0,
+              }}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography variant="bodySmallBold">Chama</Typography>
+              <Typography variant="bodySmallBold">Chama 1</Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography variant="bodySmallBold">Amount</Typography>
+              <Typography variant="bodySmallBold">4000</Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography variant="bodySmallBold">Interest</Typography>
+              <Typography variant="bodySmallBold">200</Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography variant="bodySmallBold">Due</Typography>
+              <Typography variant="bodySmallBold">12/12/2021</Typography>
+            </Box>
+          </Box>
         </Card>
       </Grid>
     </Grid>
