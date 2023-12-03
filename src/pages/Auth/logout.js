c6d0b40703e230604396d8a3855a051fc990fbb2
Allan Juma <allancjuma@gmail.com>
2023-12-03 23:18:13 +0300

    update

--- src/pages/Auth/logout.js
+++ src/pages/Auth/logout.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { selectIsLoggedIn } from "redux/features/user/userSlice";
+import { logout, selectIsLoggedIn } from "redux/features/user/userSlice";
 import { useNavigate } from "react-router-dom";
 import { apiCall } from "redux/async/asyncThunk";
 import useUserData from "Hooks/useUserData";
@@ -12,25 +12,19 @@ const Logout = () => {
   const userData = useUserData();
 
   useEffect(() => {
-    const logoutAsync = () => {
-      try {
-        isLoggedIn &&
-          dispatch(
-            apiCall({
-              endpoint: "auth/logout",
-              method: "post",
-              data: { userId: userData.userId },
-              slice: "userData",
-            })
-          );
-        localStorage.removeItem("persist:root");
-        !isLoggedIn && navigate("/login");
-      } catch (error) {
-        localStorage.removeItem("persist:root");
-      }
-    };
-
-    logoutAsync();
+    if (isLoggedIn) {
+      dispatch(
+        apiCall({
+          endpoint: "auth/logout",
+          method: "post",
+          data: { userId: userData.userId },
+          slice: "userData",
+        })
+      );
+      localStorage.removeItem("persist:root");
+      dispatch(logout());
+      navigate("/login");
+    }
   }, [dispatch, navigate, isLoggedIn, userData.userId]);
 
   return <div>Logout</div>;
