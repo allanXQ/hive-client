d00ebbe6a65e38002cb3d5f325d04a2c05f4dd04
Allan Juma <allancjuma@gmail.com>
2023-12-03 23:35:32 +0300

    unnecessary state and actions

--- src/redux/features/app/configSlice.js
+++ src/redux/features/app/configSlice.js
@@ -2,18 +2,6 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   theme: "dark",
-  topBar: {
-    height: "50px",
-  },
-  drawer: {
-    width: "200px",
-    height: "100vh",
-  },
-  messageModal: {
-    isOpen: false,
-    type: "",
-    message: "",
-  },
 };
 
 export const configSlice = createSlice({
@@ -25,17 +13,10 @@ export const configSlice = createSlice({
         ? (state.theme = "dark")
         : (state.theme = "light");
     },
-    updateMessageModal: (state, action) => {
-      state.messageModal = action.payload;
-    },
   },
 });
 
-export const { updateTheme, updateMessageModal } = configSlice.actions;
+export const { updateTheme } = configSlice.actions;
 export const selectTheme = (state) => state.config.theme;
-export const selectTopBarHeight = (state) => state.config.topBar.height;
-export const selectDrawerHeight = (state) => state.config.drawer.height;
-export const selectDrawerWidth = (state) => state.config.drawer.width;
-export const selectMessageModal = (state) => state.config.messageModal;
 
 export default configSlice.reducer;
