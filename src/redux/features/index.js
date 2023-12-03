47f2cd051ec73f90fd2335e6645744f7410b9772
Allan Juma <allancjuma@gmail.com>
2023-12-04 01:27:55 +0300

    removed config and error slice

--- src/redux/features/index.js
+++ src/redux/features/index.js
@@ -1,13 +1,9 @@
 import { combineReducers } from "redux";
 import userReducer from "./user/userSlice";
-import configReducer from "./app/configSlice";
-import errorSlice from "./app/error";
 import appSlice from "./app";
 
 const rootReducer = combineReducers({
   user: userReducer,
-  // config: configReducer,
-  // error: errorSlice,
   app: appSlice,
 });
 
