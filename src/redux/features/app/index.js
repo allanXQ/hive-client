a432c638385fbc9b5cac476ddef85cde71ea302b
Allan Juma <allancjuma@gmail.com>
2023-12-04 19:26:21 +0300

    clear message

--- src/redux/features/app/index.js
+++ src/redux/features/app/index.js
@@ -18,13 +18,17 @@ export const appSlice = createSlice({
       state.feedback.message = action.payload.message;
       state.feedback.type = action.payload.type;
     },
+    clearFeedback: (state) => {
+      state.feedback.message = null;
+      state.feedback.type = null;
+    },
     updateTheme: (state) => {
       state.config.theme = state.config.theme === "light" ? "dark" : "light";
     },
   },
 });
 
-export const { reportFeedback, updateTheme } = appSlice.actions;
+export const { reportFeedback, updateTheme, clearFeedback } = appSlice.actions;
 export const selectFeedback = (state) => state.app.feedback;
 export const selectTheme = (state) => state.app.config.theme;
 
